0bc735175a64ef44cf730e59856db427b3f18a35
DouglasGiordano <douglasmontanhagiordano@gmail.com>
2020-10-24 01:23:08 -0300

    Init project

---
+++ src/main/java/br/com/softdesign/douglasgiordano/pollingsessionmanager/PollingSessionManagerApplication.java
@@ -0,0 +1,13 @@
+package br.com.softdesign.douglasgiordano.pollingsessionmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PollingSessionManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PollingSessionManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/softdesign/douglasgiordano/pollingsessionmanager/PollingSessionManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.softdesign.douglasgiordano.pollingsessionmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PollingSessionManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
